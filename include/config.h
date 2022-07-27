2c65f1609ac0d06edf21da791644e8d5c8ad3a35
Fredyy <frederick.behrends@gmail.com>
2022-07-27 17:45:18 +0200

    updated config.h and made the turbo-buttons an option, so it will work for both types of dual-winders

--- include/config.h
+++ include/config.h
@@ -3,24 +3,34 @@
 
 #include <Arduino.h>
 
+#define HAS_TURBO_BUTTONS 1  // add support for turbo buttons, connected to pins PIN_TURBO_BUTTON_W0 and PIN_TURBO_BUTTON_W1
 #define PERFORMANCE_OUTPUT 1 // add additional debug output about the loop performance every 25k runs
 
+// define pins used for first winder
 const int PIN_W0_A1 = 2;
 const int PIN_W0_A2 = 3;
 const int PIN_W0_B1 = 4;
 const int PIN_W0_B2 = 5;
 
-const int PIN_W1_A1 = 6;
+// define pins used for second winder
+const int PIN_W1_A1 = 6; 
 const int PIN_W1_A2 = 7;
 const int PIN_W1_B1 = 8;
 const int PIN_W1_B2 = 9;
 
-const int PIN_TURBO_BUTTON_W0 = 10;
-const int PIN_TURBO_BUTTON_W1 = 11;
+// define pins used for the turbo buttons if HAS_TURBO_BUTTONS is defined
+#ifdef HAS_TURBO_BUTTONS
+    const int PIN_TURBO_BUTTON_W0 = 10;
+    const int PIN_TURBO_BUTTON_W1 = 11;
+#endif
+
+// define pins used for the mode switch
 const int PIN_MODE_SWITCH_W0  = 12;
 const int PIN_MODE_SWITCH_W1  = 13;
-const int PIN_SWITCH_LED      = 14;
-const int PIN_POWER_SWITCH    = 15;
+
+//define pins used for the power switch
+const int PIN_POWER_SWITCH_LED = 14;
+const int PIN_POWER_SWITCH     = 15;
 
 /** 
  *  3600 / TIME_INTERVAL * 24 *  ROTATIONS_PER_INTERVAL = rotations per day
@@ -35,9 +45,9 @@ const int PIN_POWER_SWITCH    = 15;
  * 
  */
 
-const int ROTATIONS_PER_INTERVAL = 3;                // add 3 rotations every interval
-const int TIME_INTERVAL          = 5*60;             // interval to add rotations in seconds, default value : 5*60 = 5 minutes
-const int TURBO_BUTTON_ROTATIONS = 200;              // add 200 rotations everytime turbobutton is pressed
+const int ROTATIONS_PER_INTERVAL = 3;               // add 3 rotations every interval
+const int TIME_INTERVAL          = 5*60;            // interval to add rotations in seconds, default value : 5*60 = 5 minutes
+const int TURBO_BUTTON_ROTATIONS = 200;             // add 200 rotations everytime turbobutton is pressed
 
 const u_int32_t STEPPER_STEPS_PER_ROTATION = 2048;  // steps needed for 1 rotation
 const u_int32_t STEPPER_MAX_STEPS_PER_SEC  = 600;   // max speed in steps per second
--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,17 @@
 #include <TimeLib.h>
 #include <Winder.h>
 
-Led led(PIN_SWITCH_LED);
+Led led(PIN_POWER_SWITCH_LED);
 
+#ifdef HAS_TURBO_BUTTONS
 Throttle turboButtonW0(PIN_TURBO_BUTTON_W0, INPUT_PULLUP);
 Throttle turboButtonW1(PIN_TURBO_BUTTON_W1, INPUT_PULLUP);
+#endif
 
 Throttle modeSwitchW0(PIN_MODE_SWITCH_W0, INPUT_PULLUP);
 Throttle modeSwitchW1(PIN_MODE_SWITCH_W1, INPUT_PULLUP);
 
 Throttle powerSwitch(PIN_POWER_SWITCH, INPUT_PULLUP);
-
-Throttle *buttons[] = {&turboButtonW0, &turboButtonW1, &modeSwitchW0, &modeSwitchW1, &powerSwitch};
 
 Winder winder0(PIN_W0_A1, PIN_W0_A2, PIN_W0_B1, PIN_W0_B2);
 Winder winder1(PIN_W1_A1, PIN_W1_A2, PIN_W1_B1, PIN_W1_B2);
@@ -33,105 +33,115 @@ void updateEverything();
 
 // put your setup code here, to run once:
 void setup() {
-  Serial.begin(111520);
+    Serial.begin(111520);
 }
 
 // put your main code here, to run repeatedly:
 void loop() {
 
-  updateEverything();
+    updateEverything();
 
-  // add rotations if turbo-button pressed
-  if (turboButtonW0.fell()) {
-    Serial.println("TurboButtonW0 triggered");
-    led.addBlinks(3);
-    if(winder0.isRunning()){
-      winder0.stop();
-    }else{
-      winder0.addRotations(TURBO_BUTTON_ROTATIONS);
+#ifdef HAS_TURBO_BUTTONS
+    // add rotations if turbo-button pressed
+    if (turboButtonW0.fell()) {
+        Serial.println("TurboButtonW0 triggered");
+        led.addBlinks(3);
+        if(winder0.isRunning()){
+            winder0.stop();
+        }else{
+            winder0.addRotations(TURBO_BUTTON_ROTATIONS);
+        }
     }
-  }
 
-  if (turboButtonW1.fell()) {
-    Serial.println("TurboButtonW1 triggered");
-    led.addBlinks(3);
-    if(winder1.isRunning()){
-      winder1.stop();
-    }else{
-      winder1.addRotations(TURBO_BUTTON_ROTATIONS);
+    if (turboButtonW1.fell()) {
+        Serial.println("TurboButtonW1 triggered");
+        led.addBlinks(3);
+        if(winder1.isRunning()){
+            winder1.stop();
+        }else{
+            winder1.addRotations(TURBO_BUTTON_ROTATIONS);
+        }
     }
-  }
+#endif
+
 
   if(powerSwitch.read() == true){ // Power-Button on
-    led.constant(false);
+      led.constant(false);
 
-    // timer reached to add new rotations
-    uint16_t time_offset = now() - last_run;
-    if(time_offset > TIME_INTERVAL){ 
-      Serial.println("TIME_INTERVAL reached to add new rotations");
+      // timer reached to add new rotations
+      uint16_t time_offset = now() - last_run;
+      if(time_offset > TIME_INTERVAL){ 
+          Serial.println("TIME_INTERVAL reached to add new rotations");
 
-      last_run = now();
+        last_run = now();
 
-      bool modeW0State = modeSwitchW0.read();
-      bool modeW1State = modeSwitchW1.read();
-      if(modeW0State == true && modeW1State == true){
-        Serial.println("Added rotations to both winders");
-        winder0.addRotations(ROTATIONS_PER_INTERVAL);
-        winder1.addRotations(ROTATIONS_PER_INTERVAL);
-      }else if (modeW0State == false){
-        Serial.println("Added rotations to winder0");
-        winder0.addRotations(ROTATIONS_PER_INTERVAL);
-      }else if (modeW1State == false){
-        Serial.println("Added rotations to winder1");
-        winder1.addRotations(ROTATIONS_PER_INTERVAL);
-      }
+        bool modeW0State = modeSwitchW0.read();
+        bool modeW1State = modeSwitchW1.read();
+        if(modeW0State == true && modeW1State == true){
+            Serial.println("Added rotations to both winders");
+            winder0.addRotations(ROTATIONS_PER_INTERVAL);
+            winder1.addRotations(ROTATIONS_PER_INTERVAL);
+        }else if (modeW0State == false){
+            Serial.println("Added rotations to winder0");
+            winder0.addRotations(ROTATIONS_PER_INTERVAL);
+        }else if (modeW1State == false){
+            Serial.println("Added rotations to winder1");
+            winder1.addRotations(ROTATIONS_PER_INTERVAL);
+        }
 
-      led.addBlinks(3);
+          led.addBlinks(3);
 
     }else{
 
-      if(time_offset % 10 == 0 && last_run_report != time_offset){
-        char buffer[102];
-        sprintf(buffer, "Time Offset = %ds, Waiting for TIME_INTERVAL = %ds to add ROTATIONS_PER_INTERVAL = %d new rotations.", time_offset, TIME_INTERVAL, ROTATIONS_PER_INTERVAL);
-        Serial.println(buffer);
-        last_run_report = time_offset;
-      }
+        if(time_offset % 10 == 0 && last_run_report != time_offset){
+            char buffer[102];
+            sprintf(buffer, "Time Offset = %ds, Waiting for TIME_INTERVAL = %ds to add ROTATIONS_PER_INTERVAL = %d new rotations.", time_offset, TIME_INTERVAL, ROTATIONS_PER_INTERVAL);
+            Serial.println(buffer);
+            last_run_report = time_offset;
+        }
 
     }
 
   }else{
-    led.constant(true);
+      led.constant(true);
   }
 
 
-  #ifdef PERFORMANCE_OUTPUT
+#ifdef PERFORMANCE_OUTPUT
   runCounter++;
-  if(runCounter == outputSteps){
-    uint32_t elapsedTime = millis() - startTime; 
-    double avg = (double)elapsedTime / (double)outputSteps;
-    double freq = (1000.0/ (double)elapsedTime) * (double)outputSteps;
+    if(runCounter == outputSteps){
+        uint32_t elapsedTime = millis() - startTime; 
+        double avg = (double)elapsedTime / (double)outputSteps;
+        double freq = (1000.0/ (double)elapsedTime) * (double)outputSteps;
 
-    char buffer[100];
-    sprintf(buffer, "Performance Output: %ld loop runs, elapsed %ld ms total, average %s ms per loop, loop-freq: %shz", outputSteps, elapsedTime, String(avg, 5).c_str(), String(freq, 2).c_str());
-    Serial.println(buffer);
+        char buffer[100];
+        sprintf(buffer, "Performance Output: %ld loop runs, elapsed %ld ms total, average %s ms per loop, loop-freq: %shz", outputSteps, elapsedTime, String(avg, 5).c_str(), String(freq, 2).c_str());
+        Serial.println(buffer);
 
-    startTime = millis();
-    runCounter = 0;
+        startTime = millis();
+        runCounter = 0;
   }
-  #endif
+#endif
 
 }
 
 void updateEverything(){
-  // update LED to make it blink, when needed.
-  led.update();
+    // update LED to make it blink, when needed.
+    led.update();
 
-  // update Buttons/Switch
-  for (Throttle *button: buttons) {
-    button->update();
-  }
+    // update Buttons/Switch
+    modeSwitchW0.update();
+    modeSwitchW1.update();
+    powerSwitch.update();
 
-  // update winders
-  winder0.update();
-  winder1.update();
+#ifdef HAS_TURBO_BUTTONS
+    turboButtonW0.update();
+    turboButtonW1.update();
+#endif
+
+
+
+    // update winders
+    winder0.update();
+    winder1.update();
 }
