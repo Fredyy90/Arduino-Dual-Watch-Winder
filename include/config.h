4921eefc4d6e625bfc69de3aa747ce28b223a096
Fredyy <frederick.behrends@gmail.com>
2022-07-27 18:40:30 +0200

    the soft power button and led are now also option to be configured in config.h

--- include/config.h
+++ include/config.h
@@ -3,8 +3,10 @@
 
 #include <Arduino.h>
 
-#define HAS_TURBO_BUTTONS 1  // add support for turbo buttons, connected to pins PIN_TURBO_BUTTON_W0 and PIN_TURBO_BUTTON_W1
-#define PERFORMANCE_OUTPUT 1 // add additional debug output about the loop performance every 25k runs
+//#define HAS_TURBO_BUTTONS 1      // add support for turbo buttons, connected to pins PIN_TURBO_BUTTON_W0 and PIN_TURBO_BUTTON_W1
+//#define HAS_SOFT_POWER_SWITCH 1  // add support for "soft" power switch, connected to pin PIN_POWER_SWITCH and not directly to the power supply
+//#define HAS_LED 1                // uncomment if you connect a LED to pin PIN_POWER_SWITCH_LED e.g. the LED in the power switch
+//#define PERFORMANCE_OUTPUT 1     // add additional debug output about the loop performance every 25k runs
 
 // define pins used for first winder
 const int PIN_W0_A1 = 2;
@@ -19,7 +21,7 @@ const int PIN_W1_B1 = 8;
 const int PIN_W1_B2 = 9;
 
 // define pins used for the turbo buttons if HAS_TURBO_BUTTONS is defined
-#ifdef HAS_TURBO_BUTTONS
+#if defined(HAS_TURBO_BUTTONS)
     const int PIN_TURBO_BUTTON_W0 = 10;
     const int PIN_TURBO_BUTTON_W1 = 11;
 #endif
@@ -28,9 +30,14 @@ const int PIN_W1_B2 = 9;
 const int PIN_MODE_SWITCH_W0  = 12;
 const int PIN_MODE_SWITCH_W1  = 13;
 
-//define pins used for the power switch
-const int PIN_POWER_SWITCH_LED = 14;
-const int PIN_POWER_SWITCH     = 15;
+#if defined(HAS_LED)
+    const int PIN_POWER_SWITCH_LED = 14;
+#endif
+
+#if defined(HAS_SOFT_POWER_SWITCH)
+    //define pins used for the power switch
+    const int PIN_POWER_SWITCH     = 15;
+#endif
 
 /** 
  *  3600 / TIME_INTERVAL * 24 *  ROTATIONS_PER_INTERVAL = rotations per day
@@ -53,4 +60,10 @@ const u_int32_t STEPPER_STEPS_PER_ROTATION = 2048;  // steps needed for 1 rotati
 const u_int32_t STEPPER_MAX_STEPS_PER_SEC  = 600;   // max speed in steps per second
 const u_int32_t STEPPER_ACCELERATION       = 2048;  // in steps*second²
 
+
+#if !defined(HAS_LED) && defined(LED_BUILTIN)
+    #define HAS_LED 1
+    const int PIN_POWER_SWITCH_LED = LED_BUILTIN;
 #endif
+
+#endif // CONFIG_h
--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,11 @@
 #include <TimeLib.h>
 #include <Winder.h>
 
+#if defined(HAS_LED)
 Led led(PIN_POWER_SWITCH_LED);
+#endif
 
-#ifdef HAS_TURBO_BUTTONS
+#if defined(HAS_TURBO_BUTTONS)
 Throttle turboButtonW0(PIN_TURBO_BUTTON_W0, INPUT_PULLUP);
 Throttle turboButtonW1(PIN_TURBO_BUTTON_W1, INPUT_PULLUP);
 #endif
@@ -15,7 +17,9 @@ Throttle turboButtonW1(PIN_TURBO_BUTTON_W1, INPUT_PULLUP);
 Throttle modeSwitchW0(PIN_MODE_SWITCH_W0, INPUT_PULLUP);
 Throttle modeSwitchW1(PIN_MODE_SWITCH_W1, INPUT_PULLUP);
 
+#if defined(HAS_SOFT_POWER_SWITCH)
 Throttle powerSwitch(PIN_POWER_SWITCH, INPUT_PULLUP);
+#endif
 
 Winder winder0(PIN_W0_A1, PIN_W0_A2, PIN_W0_B1, PIN_W0_B2);
 Winder winder1(PIN_W1_A1, PIN_W1_A2, PIN_W1_B1, PIN_W1_B2);
@@ -23,7 +27,7 @@ Winder winder1(PIN_W1_A1, PIN_W1_A2, PIN_W1_B1, PIN_W1_B2);
 uint32_t last_run = now();
 uint32_t last_run_report = 0;
 
-#ifdef PERFORMANCE_OUTPUT
+#if defined(PERFORMANCE_OUTPUT)
 uint32_t startTime  = millis();
 uint32_t runCounter = 0;
 const uint32_t outputSteps = 25000;
@@ -41,37 +45,51 @@ void loop() {
 
     updateEverything();
 
-#ifdef HAS_TURBO_BUTTONS
-    // add rotations if turbo-button pressed
-    if (turboButtonW0.fell()) {
-        Serial.println("TurboButtonW0 triggered");
-        led.addBlinks(3);
-        if(winder0.isRunning()){
-            winder0.stop();
-        }else{
-            winder0.addRotations(TURBO_BUTTON_ROTATIONS);
+    #if defined(HAS_TURBO_BUTTONS)
+        // add rotations if turbo-button pressed
+        if (turboButtonW0.fell()) {
+            Serial.println("TurboButtonW0 triggered");
+
+            #if defined(HAS_LED)
+                led.addBlinks(3);
+            #endif
+            if(winder0.isRunning()){
+                winder0.stop();
+            }else{
+                winder0.addRotations(TURBO_BUTTON_ROTATIONS);
+            }
         }
-    }
 
-    if (turboButtonW1.fell()) {
-        Serial.println("TurboButtonW1 triggered");
-        led.addBlinks(3);
-        if(winder1.isRunning()){
-            winder1.stop();
-        }else{
-            winder1.addRotations(TURBO_BUTTON_ROTATIONS);
+        if (turboButtonW1.fell()) {
+            Serial.println("TurboButtonW1 triggered");
+            #if defined(HAS_LED)
+                led.addBlinks(3);
+            #endif
+            if(winder1.isRunning()){
+                winder1.stop();
+            }else{
+                winder1.addRotations(TURBO_BUTTON_ROTATIONS);
+            }
         }
-    }
-#endif
+    #endif
 
+    #if defined(HAS_SOFT_POWER_SWITCH)
+        if(powerSwitch.read() == false){ // Power-Button off
+            #if defined(HAS_LED)
+            led.constant(true);
+            #endif
+            return;
+        }
+    #endif
 
-  if(powerSwitch.read() == true){ // Power-Button on
-      led.constant(false);
+    #if defined(HAS_LED)
+        led.constant(false);
+    #endif
 
-      // timer reached to add new rotations
-      uint16_t time_offset = now() - last_run;
-      if(time_offset > TIME_INTERVAL){ 
-          Serial.println("TIME_INTERVAL reached to add new rotations");
+    // timer reached to add new rotations
+    uint16_t time_offset = now() - last_run;
+    if(time_offset > TIME_INTERVAL){ 
+        Serial.println("TIME_INTERVAL reached to add new rotations");
 
         last_run = now();
 
@@ -89,7 +107,9 @@ void loop() {
             winder1.addRotations(ROTATIONS_PER_INTERVAL);
         }
 
-          led.addBlinks(3);
+        #if defined(HAS_LED)
+            led.addBlinks(3);
+        #endif
 
     }else{
 
@@ -102,42 +122,43 @@ void loop() {
 
     }
 
-  }else{
-      led.constant(true);
-  }
 
+    #if defined(PERFORMANCE_OUTPUT)
+        runCounter++;
+        if(runCounter == outputSteps){
+            uint32_t elapsedTime = millis() - startTime; 
+            double avg = (double)elapsedTime / (double)outputSteps;
+            double freq = (1000.0/ (double)elapsedTime) * (double)outputSteps;
 
-#ifdef PERFORMANCE_OUTPUT
-  runCounter++;
-    if(runCounter == outputSteps){
-        uint32_t elapsedTime = millis() - startTime; 
-        double avg = (double)elapsedTime / (double)outputSteps;
-        double freq = (1000.0/ (double)elapsedTime) * (double)outputSteps;
+            char buffer[100];
+            sprintf(buffer, "Performance Output: %ld loop runs, elapsed %ld ms total, average %s ms per loop, loop-freq: %shz", outputSteps, elapsedTime, String(avg, 5).c_str(), String(freq, 2).c_str());
+            Serial.println(buffer);
 
-        char buffer[100];
-        sprintf(buffer, "Performance Output: %ld loop runs, elapsed %ld ms total, average %s ms per loop, loop-freq: %shz", outputSteps, elapsedTime, String(avg, 5).c_str(), String(freq, 2).c_str());
-        Serial.println(buffer);
-
-        startTime = millis();
-        runCounter = 0;
-  }
-#endif
+            startTime = millis();
+            runCounter = 0;
+        }
+    #endif
 
 }
 
 void updateEverything(){
-    // update LED to make it blink, when needed.
-    led.update();
+
+    #if defined(HAS_LED)
+        led.update();
+    #endif
 
     // update Buttons/Switch
     modeSwitchW0.update();
     modeSwitchW1.update();
-    powerSwitch.update();
 
-#ifdef HAS_TURBO_BUTTONS
-    turboButtonW0.update();
-    turboButtonW1.update();
-#endif
+    #if defined(HAS_SOFT_POWER_SWITCH)
+        powerSwitch.update();
+    #endif
+
+    #if defined(HAS_TURBO_BUTTONS)
+        turboButtonW0.update();
+        turboButtonW1.update();
+    #endif
 
 
 
